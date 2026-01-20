a4f6246e1502434105e72064f8fed12c27123346
Your Name <you@example.com>
2026-01-20 11:01:27 -0800

    adds target

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # moon
-# moon
-# moon
+
+if a file(s) was added
+
+'git add .'
+
+to make a commit / save point
+
+' git commit -am' 'useful message here'
+
+to push/upload
+
+'git push'
--- sketch.js
+++ sketch.js
@@ -4,4 +4,12 @@ function setup() {
 
 function draw() {
   background(220);
+  drawTarget(width/2, height/2);
+  ellipse(width/2, height/2, 100, 100);
+  ellipse(width/2, height/2, 66, 66);
+  ellipse(width/2, height/2, 33, 33);
+}
+
+function drawTarget(x, y) {
+
 }
